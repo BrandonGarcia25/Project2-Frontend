067a2eca81fba523dd82624a18cfba442e9f05f5
BrandonGarcia25 <bgbrandongarcia@gmail.com>
2023-12-20 17:18:52 -0400

    updated Github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import githubLogo from "../assets/githubLogo-white.png";
 const Footer = () => {
   return (
     <div className="Footer">
-      <a href="https://github.com/BrandonGarcia25/e-commerce-local-coffee">
+      <a href="https://github.com/BrandonGarcia25/Project2-Frontend">
         <p><img src={githubLogo} alt="githubLogo" className="githubLogo" /> GitHub repository </p>
       </a>
     </div>
