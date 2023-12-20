97ef6b28b6ec98723e6a83db84897814dcb56a35
BrandonGarcia25 <bgbrandongarcia@gmail.com>
2023-12-20 13:50:20 -0400

    changed API

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,5 +1,5 @@
 //export const API_URL = '"https://fake-coffee-api.vercel.app/api"'
 
-// export const API_URL = 'https://json-server-review-backend.adaptable.app'
+export const API_URL = 'https://json-server-review-backend.adaptable.app'
 
-export const API_URL = 'http://localhost:5005'
+// export const API_URL = 'http://localhost:5005'
